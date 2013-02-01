c1e36465f336a796473b1bf8b5ade6ef55822d2f
root <root@rackspace-e-learing.com>
2013-02-01 21:29:08 +0000

    rename files

---
+++ lang/en/qtype_cloud.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qtype_cloud', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package    qtype
+ * @subpackage cloud
+ * @copyright  2013 Chris Brucks
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['informationtext'] = 'Information text';
+$string['pluginname'] = 'Cloud';
+$string['pluginname_help'] = 'A cloud type is not really a question. It provides a way for someone to give a user access information to a virtual server and give them a related walk through section.';
+$string['pluginnameadding'] = 'Adding a Cloud';
+$string['pluginnameediting'] = 'Editing a Cloud';
+$string['pluginnamesummary'] = 'This is not actually a question. Instead it is a way to give a user access info for a virtual server and walk them through some tasks.';
