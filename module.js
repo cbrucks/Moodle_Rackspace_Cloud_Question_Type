24cdf814d9a0936355760a2c585f9bc8aaf9fffb
Chris Brucks <cbrucks@gmail.com>
2013-02-25 15:27:34 -0600

    seperate curl call function

--- module.js
+++ module.js
@@ -17,10 +17,48 @@ M.qtype_cloud = {
         var ipaddress = '';
         
         YUI.add("uuu", function (Y) {
-            var funcs = {
-                doSomething: function () { window.alert("Done."); }
+            var functions = {
+                getServerInfo: function (Y, server_id) {
+                    var callback = {
+                        timeout : 5000,
+
+                        responseType: 'json',
+     
+                        on : {
+                            success : function (x,o) {
+                                
+                                Y.log("RAW JSON DATA: " + o.responseText);
+
+                                // Process the JSON data returned from the server
+                                try {
+                                    info = Y.JSON.parse(o.responseText);
+                                }
+                                catch (e) {
+                                    Y.log("JSON Parse failed!");
+                                    return NULL;
+                                }
+                                
+                                return info;
+
+                            },
+                            failure : function (x,o) {
+                                Y.log("Async call failed!");
+                                return NULL;
+                            }
+                        },
+                        
+                        method: 'GET',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Accept': 'application/json',
+                        },
+                    };
+                    
+                    Y.io(location.protocol + '//' + location.host + '/question/type/cloud/getipaddress.php?url=' + base_url + server_id + '&command_type=GET&extra_headers[]=X-Auth-Token:' + auth_token, callback);
+                }
             };
-            Y.theObject = funcs;
+            
+            Y.server = functions;
         });
 
 
@@ -31,99 +69,67 @@ M.qtype_cloud = {
                 
                 var target = Y.one('.' + server_info["class"]);
 
-                // Create the io callback/configuration
-                var callback = {
-                    timeout : 5000,
-
-                    responseType: 'json',
- 
-                    on : {
-                        success : function (x,o) {
-                            if (target) {
-                            
-                                Y.log("RAW JSON DATA: " + o.responseText);
-
-                                // Process the JSON data returned from the server
-                                try {
-                                    info = Y.JSON.parse(o.responseText);
-                                }
-                                catch (e) {
-                                    target.setContent("JSON Parse failed!");
-                                    handle[handle_i].cancel();
-                                    return;
-                                }
-
-                                if (info === undefined) {
-                                    target.setContent("Problem with settings sent to php script.");
-                                    handle[handle_i].cancel();
-                                }
-
-                                if (info.itemNotFound !== undefined && info.itemNotFound.message !== undefined) {
-                                    target.setContent("Failed: " + info.itemNotFound.message + "    Code:" + info.itemNotFound.code);
-                                    handle[handle_i].cancel();
-                                }
-                            
-                                switch (info.server.status) {
- 
-                                    case 'reuse':
-                                        break;
-                                    
-                                    case 'new':
-                                        break;
-
-                                    case 'BUILD':
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
-                                        if (ipaddress.length === 0 && info.server !== undefined && info.server.addresses !== undefined && info.server.addresses.public !== undefined) {
-                                            for (i=0; i<info.server.addresses.public.length; i++) {
-                                                if (info.server.addresses.public[i].version == 4) {
-                                                    ipaddress = info.server.addresses.public[i].addr;
-                                                    break;
-                                                }
-                                            }
-
-                                            // replace all environment variables
-                                            body = Y.one(document.body);
-                                            var body_text = body.getContent();
-                                            body_text = body_text.split("[%=" + server_info["class"] + "%]").join(ipaddress);
-                                            body.setContent(body_text);
-
-                                            // reveal the question text with the IP environment variable replaced
-//                                            Y.one(".qtext").setStyle('display', 'inline');
-                                        }
-                                        break;
-                                        
-                                    case 'PASSWORD':
-                                        break;
-
-                                    case 'ACTIVE':
-                                        // wait a few cycles to ensure that the process is done
-                                        break;
-
-                                    default:
-                                        break;
-                                }
-                            } else {
-                                target.setContent("Could Not Find the Server IP field for server number " + (handle_i+1));
-                                handle[handle_i].cancel();
-                                return;
-                            }
-                        },
-                        failure : function (x,o) {
-                            target.setContent("Async call failed!");
-                            handle[handle_i].cancel();
-                            return;
-                        }
-                    },
-                    
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Accept': 'application/json',
-                    },
-                };
+                if (target) {
                 
-                Y.io(location.protocol + '//' + location.host + '/question/type/cloud/getipaddress.php?url=' + base_url + server_info['id'] + '&command_type=GET&extra_headers[]=X-Auth-Token:' + auth_token, callback);
-            }
+                    info = Y.server.getServerInfo(Y, server_info['id']);
+
+                    if (info === undefined) {
+                        target.setContent("Problem with settings sent to php script.");
+                        handle[handle_i].cancel();
+                    }
+
+                    if (info.itemNotFound !== undefined && info.itemNotFound.message !== undefined) {
+                        target.setContent("Failed: " + info.itemNotFound.message + "    Code:" + info.itemNotFound.code);
+                        handle[handle_i].cancel();
+                    }
+                
+                    switch (info.server.status) {
+
+                        case 'reuse':
+                            break;
+                        
+                        case 'new':
+                            break;
+
+                        case 'BUILD':
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+                            if (ipaddress.length === 0 && info.server !== undefined && info.server.addresses !== undefined && info.server.addresses.public !== undefined) {
+                                for (i=0; i<info.server.addresses.public.length; i++) {
+                                    if (info.server.addresses.public[i].version == 4) {
+                                        ipaddress = info.server.addresses.public[i].addr;
+                                        break;
+                                    }
+                                }
+
+                                // replace all environment variables
+                                body = Y.one(document.body);
+                                var body_text = body.getContent();
+                                body_text = body_text.split("[%=" + server_info["class"] + "%]").join(ipaddress);
+                                body.setContent(body_text);
+
+                                // reveal the question text with the IP environment variable replaced
+//                                            Y.one(".qtext").setStyle('display', 'inline');
+                            }
+                            break;
+                            
+                        case 'PASSWORD':
+                            break;
+
+                        case 'ACTIVE':
+                            // wait a few cycles to ensure that the process is done
+                            break;
+
+                        default:
+                            break;
+                    }
+                } else {
+                    target.setContent("Could Not Find the Server IP field for server number " + (handle_i+1));
+                    handle[handle_i].cancel();
+                    return;
+                }
+            };
+                
+            Y.io(location.protocol + '//' + location.host + '/question/type/cloud/getipaddress.php?url=' + base_url + server_info['id'] + '&command_type=GET&extra_headers[]=X-Auth-Token:' + auth_token, callback);
         });
         
         var handle = new Array();
